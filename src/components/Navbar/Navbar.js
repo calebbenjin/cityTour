ade451268843c9ede18a9442ddecf8ed69b150e9
kaiylebdev <calebben94@gmail.com>
2020-03-24 12:33:00 +0100

    add setUp

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './navbar.scss';
+import logo from '../../logo.svg';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="city tours company" />
+      <ul className="nav-links">
+        <li>
+          <a href="/" className="nav-link">Home</a>
+        </li>
+        <li>
+          <a href="/" className="nav-link">About</a>
+        </li>
+        <li>
+          <a href="/" className="nav-link">Tours</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
