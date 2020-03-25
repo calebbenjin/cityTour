3af07a3b45c9b7b4c82cfceeaf0f519970acc3c1
kaiylebdev <calebben94@gmail.com>
2020-03-25 15:13:06 +0100

    app functionality

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 import Tour from '../Tour';
+import './tourlist.scss';
+import {tourData} from '../../tourData';
+
+
+
 
 export default class TourList extends Component {
+  state = {
+    tours: tourData
+  };
+  removeTour = (id) => {
+    const sortTours = this.state.tours.filter((tour)=> tour.id !== id)
+    this.setState({
+      tours: sortTours
+    })
+  }
   render() {
+    const {tours} = this.state;
     return (
-      <div>
-        Hello from TourList
-        <Tour />
-      </div>
+      <section className="tourlist">
+       {
+         tours.map(tour => (<Tour key={tour.id} tours={tour} removeTour={this.removeTour} />))
+       }
+      </section>
     )
   }
 }
